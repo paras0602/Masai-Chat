14adf9df7126d35a4f63a7fdbbb7698adc261bba
paras0602 <parasjain912@gmail.com>
2022-07-28 15:29:53 +0530

    code added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-
-import './App.css';
-
-function App() {
+import React , {useEffect} from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import "antd/dist/antd.css";
+import "font-awesome/css/font-awesome.min.css";
+import Footer from "./components/Footer/Footer";
+const App = () => {
+  
   return (
-    <div className="App">
-    
-    </div>
+
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+      <Footer />
+    </Router>
+
   );
-}
+};
 
 export default App;
